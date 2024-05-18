ab0688ff65dc32e109f0cc4754ed00bd852b185f
Humaira Moin <159626452+humamoin70@users.noreply.github.com>
2024-05-17 21:38:34 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,39 @@
+// let names:string [] = ["Ayesha","Barera","Zain","Huma"]
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+
+
+// let names1:string [] = ["Ayesha","Barera","Zain","Huma"]
+// names1 [1] = "Arham";
+// console.log(names1);
+
+// let names1:string [] = ["Ayesha","Barera","Zain","Huma"]
+// names1.pop()
+// console.log(names1)
+
+// let fruits1:string [] = ["Mango","Apple","Pineapple","lemon"];
+// fruits1.shift()
+// console.log(fruits1);
+
+// let fruits1:string [] = ["Mango","Apple","Pineapple","lemon"];
+// fruits1.push("pear")
+// console.log(fruits1);
+
+
+
+
+// let animal:string [] = [ "cock","zebra","Hen","Duck"]
+// animal.unshift("chick");
+// console.log(animal)
+
+// let planet:string []= ["Mars","Sun","Earth","Jupiter"]
+// // planet.slice(1,3)
+// // console.log(planet);
+//  let newPlanet = planet.slice(1,3)
+// console.log(newPlanet);
+
+let planet:string []= ["Mars","Sun","Earth","Jupiter"]
+planet.splice(1,3, "Moon","Mercury","Orbit");
+console.log(planet);
---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+//if we want to delete an item in the array from start or end typescript provides us two methods 
+// Pop:Pop is use to delete an item from end of the array
+//Shift :Shift is use to delete an item from biginning of the array
+//Example 1
+let fruits = ["App;e", "Mango", "banana"];
+console.log("original array:", fruits); //Output:Original array:["Apple","Mango","Banana"]
+//delete an element from biginning of the array using
+fruits.pop();
+console.log("Array after pop:", fruits); //Output:Array after pop["Apple","Mango",
